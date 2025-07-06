d9ea45f9bbff49d9eb729c001b3f1c85d16531b7
angelopelomundo <111236826+angelopelomundo@users.noreply.github.com>
2025-07-06 10:49:23 -0300

    Update cria_estrutura.shcreate

--- cria_estrutura.sh
+++ cria_estrutura.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+mkdir -p app/src/main/java/com/angelgrid
+mkdir -p app/src/main/res/layout
+mkdir -p app/src/main/res/values
+mkdir -p app/src/main/res/mipmap-anydpi-v26
+mkdir -p app/src/main/res/mipmap-hdpi
+mkdir -p app/src/main/res/mipmap-mdpi
+mkdir -p app/src/main/res/mipmap-xhdpi
+mkdir -p app/src/main/res/mipmap-xxhdpi
+mkdir -p app/src/main/res/mipmap-xxxhdpi
 
+touch app/src/main/AndroidManifest.xml
+touch app/src/main/java/com/angelgrid/MainActivity.kt
+touch app/src/main/res/layout/activity_main.xml
+touch app/src/main/res/values/colors.xml
+touch app/src/main/res/values/themes.xml
+touch app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
+touch app/src/main/res/mipmap-hdpi/ic_launcher.png
+touch app/src/main/res/mipmap-mdpi/ic_launcher.png
+touch app/src/main/res/mipmap-xhdpi/ic_launcher.png
+touch app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+touch app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
